2be95463bf2647e0fd9ee290c9fc1771aac79382
Tommaso Giammaria <tommaso.giammaria1412@gmail.com>
2025-05-29 11:30:18 +0200

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -7,15 +7,18 @@ from draw_utilities import *
 
 black = 0, 0, 0
 white = 255, 255, 255
-red = 255, 0, 0
-green = 0, 255, 0
-blue = 0, 0, 255
-yellow = 255, 255, 0
-cyan = 0, 255, 255
-magenta = 255, 0, 255
+
 gray = 128, 128, 128
 dark_gray = 64, 64, 64
 light_gray = 192, 192, 192
+
+red = 255, 0, 0
+green = 0, 255, 0
+blue = 0, 0, 255
+
+yellow = 255, 255, 0
+cyan = 0, 255, 255
+magenta = 255, 0, 255
 
 class Game_engine:
     """
